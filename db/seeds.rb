2bf3e862165f07b9d69883b85cad6ec4160c7b2f
Andy Delgado <eandrewdelgado@gmail.com>
2017-06-03 12:23:14 -0400

    Create seed file for test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+	Blog.create!(
+		title: "My Blog Post #{blog}",
+		body: "asd;flkasdjf; as;dlkfjas df;lkjasdf;aklsdjf asd;faklsjdf a;sldk
+		a;slkdfja;sdlkfjas;dlkja a;sdlkfjas;dfkaj a;sldkfjas;dfkj"
+	)
+end
+
+puts "10 blog posts created"
+
+5.times do |skill|
+	Skill.create!(
+		title: "Rails #{skill}",
+		percent_utilized: 15
+	)
+end
+
+puts "5 skills created"
+
+9.times do |project|
+	Project.create!(
+		title: "Project #{project}",
+		subtitle: "My great service",
+		body: "asd;flkj as;dlkfjasd; a;sldkjfas;dlkfj",
+		main_image: "http://via.placeholder.com/600x400",
+		thumb_image: "http://via.placeholder.com/350x200" 
+	)
+end
+puts "9 projects created"
