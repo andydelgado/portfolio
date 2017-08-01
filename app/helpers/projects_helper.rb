12b2a290a5f9bf377641408f891a2f91413b957a
Andy Delgado <eandrewdelgado@gmail.com>
2017-07-31 21:34:27 -0400

    Fix placeholder images

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,15 @@
 module ProjectsHelper
+  def image_generator(height:, width:)
+    "http://placehold.it/#{height}x#{width}"
+  end
+
+  def project_img img, type
+    if img.model.main_image? || img.model.thumb_image?
+      img
+    elsif type == 'thumb'
+      image_generator(height: '350', width: '200')
+    else
+      image_generator(height: '600', width: '350')
+    end
+  end
 end
